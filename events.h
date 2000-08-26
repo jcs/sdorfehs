0091294093e1703832394a1d5659bb6508a0bc98
sabetts <sabetts>
2000-08-26 06:03:41 +0000

    added copyright notice.

--- events.h
+++ events.h
@@ -1,4 +1,21 @@
-/* Function prototypes */
+/* Function prototypes
+ *  
+ * Copyright (C) 2000 Shawn Betts
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307 USA */
 
 #ifndef _EVENTS_H
 #define _EVENTS_H
