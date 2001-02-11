3b43684c95a2e1a07000b50e92f38a4e3dd33457
rcyeske <rcyeske>
2001-02-11 04:59:40 +0000

    (ratpoison_SOURCES): added messages.h

---
+++ src/messages.h
@@ -0,0 +1,4 @@
+#define MESSAGE_NO_OTHER_WINDOW "No other window."
+#define MESSAGE_NO_MANAGED_WINDOWS "No managed windows."
+#define MESSAGE_PROMPT_GOTO_WINDOW_NAME "Switch to window: "
+#define MESSAGE_PROMPT_NEW_WINDOW_NAME "Set window's title to: "
