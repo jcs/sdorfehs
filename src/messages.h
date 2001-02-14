cdfd5a7e445f5f93d946d5b9d1d5cf1f5b6efd62
rcyeske <rcyeske>
2001-02-14 03:43:35 +0000

    updated language

--- src/messages.h
+++ src/messages.h
@@ -1,4 +1,14 @@
-#define MESSAGE_NO_OTHER_WINDOW "No other window."
-#define MESSAGE_NO_MANAGED_WINDOWS "No managed windows."
-#define MESSAGE_PROMPT_GOTO_WINDOW_NAME "Switch to window: "
-#define MESSAGE_PROMPT_NEW_WINDOW_NAME "Set window's title to: "
+#ifndef _MESSAGES_H
+#define _MESSAGES_H
+
+#define MESSAGE_NO_OTHER_WINDOW " No other window "
+#define MESSAGE_NO_MANAGED_WINDOWS " No managed windows "
+#define MESSAGE_UNKNOWN_COMMAND ": unknown command "
+
+#define MESSAGE_PROMPT_GOTO_WINDOW_NAME " Switch to window: "
+#define MESSAGE_PROMPT_NEW_WINDOW_NAME " Set window's title to: "
+#define MESSAGE_PROMPT_SHELL_COMMAND "/bin/sh -c "
+#define MESSAGE_PROMPT_COMMAND ":"
+#define MESSAGE_PROMPT_SWITCH_WM " Switch to wm: "
+
+#endif
