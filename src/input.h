69f6de138ac7ec7cb8be9de3c584e82580dbb2ae
algernon <algernon>
2001-02-17 01:42:33 +0000

    reflect get_input chage

--- src/input.h
+++ src/input.h
@@ -1,22 +1,30 @@
-/* 
- * Copyright (C) 2000 Shawn Betts
- * 
- * This program is free software; you can redistribute it and/or modify
+/* Function prototypes.
+ * Copyright (C) 2000, 2001 Shawn Betts
+ *
+ * This file is part of ratpoison.
+ *
+ * ratpoison is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
- * 
- * This program is distributed in the hope that it will be useful,
+ *
+ * ratpoison is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307 USA */
+ * Boston, MA 02111-1307 USA
+ */
+
+#ifndef _RATPOISON_INPUT_H
+#define _RATPOISON_INPUT_H 1
 
 char *keysym_to_string (KeySym keysym, unsigned int modifier);
 void cook_keycode (KeyCode keycode, KeySym *keysym, unsigned int *mod);
-void get_input (screen_info *s, char *prompt, char *str, int len);
+char *get_input (screen_info *s, char *prompt);
 void read_key (KeySym *keysym, unsigned int *mode);
+
+#endif /* ! _RATPOISON_INPUT_H */
