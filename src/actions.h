c018fca46e53345957a00ee30d96409e8fcaff1b
algernon <algernon>
2001-02-17 01:43:15 +0000

    copyright year update & stuff

--- src/actions.h
+++ src/actions.h
@@ -1,22 +1,26 @@
-/* 
- * Copyright (C) 2000 Shawn Betts
- * 
- * This program is free software; you can redistribute it and/or modify
+/* Prototypes of all actions that can be performed with keystrokes.
+ * Copyright (C) 2000, 2001 Shawn Betts
+ *
+ * This file is part of ratpoison.
+ *
+ * ratpoison is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
- * 
- * This program is distributed in the hope that it will be useful,
+ *
+ * ratpoison is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307 USA */
+ * Boston, MA 02111-1307 USA
+ */
 
-/* Prototypes of all actions that can be performed with keystrokes. */
+#ifndef _RATPOISON_ACTIONS_H
+#define _RATPOISON_ACTIONS_H 1
 
 #define MAX_COMMAND_LENGTH 100
 #define MAX_ARGS_LENGTH 100
@@ -57,3 +61,5 @@ void show_version (void *data);
 void xterm_command (void *data);
 
 extern rp_action key_actions[];
+
+#endif /* ! _RATPOISON_ACTIONS_H */
