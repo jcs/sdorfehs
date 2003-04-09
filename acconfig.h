a70ebaf426cc94a6832e7f9eb6335012af595f0a
sabetts <sabetts>
2003-04-09 23:12:10 +0000

    (TERM_PROG): Add doc string

--- acconfig.h
+++
@@ -1,9 +0,0 @@
-/* When defined, ratpoison will print debugging information */
-#undef DEBUG
-
-/* X terminal emulator to user */
-#undef TERM_PROG
-
-/* name of emacs program */
-#undef EMACS_PROG
-
