a4a72482a7944080c6737a436966368faa058078
sabetts <sabetts>
2004-10-07 16:36:57 +0000

    Add a -f option to forcefully create symlinks of missing files.

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-aclocal && autoheader && automake -a -c && autoconf
+# usage: ./autogen.sh [-f]
+# option "-f" means forcefully create symlinks for missing files
+#                   (by default: copies are made only if necessary)
+
+if [ x"$1" = x-f ]
+  then shift ; am_opt='--force'
+  else         am_opt='--copy'
+fi
+
+aclocal && autoheader && automake -a $am_opt && autoconf
+
