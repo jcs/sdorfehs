5304bcb44ff71ddfa02af3bff4d05812b4fc4be3
Jérémie Courrèges-Anglas <jca@wxcvbn.org>
2013-11-17 17:54:07 +0100

    Revert "Added contrib/ratdate.sh and Makefile for building"
    This reverts commit 2e7e99fc780c4099861039ebaab0fb8629d7601b.
    
    Problems:
    - no copyright statement
    - CC BY-ND considered undesirable by myself.

--- contrib/ratdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# License: CC BY-ND 3.0 see http://creativecommons.org/licenses/by-nd/3.0
-#
-# Description:	This script displays the date with a calendar indicating today
-#		via ratpoison's echo command.
-# Suggest rebinding :time as follows: bind a exec exec ratdate.sh
-today=`date +%e`
-cal=`cal -h | tail -n +2 | sed -e 's/^Su/\n Su/' -e 's/.*/ & /' -e "s/\ $today\ /\<$today\>/"`
-exec ratpoison -c "echo `date +' %r - %A %n    %D - %B'` $cal"
